64f0aa607f628a6b689255cc1d9450394ffc50ac
Mike Slinn <mslinn@micronauticsresearch.com>
2016-05-16 09:29:10 -0700

    Declared a few types

--- src/main/scala/net/debasishg/snippet/cake/Main.scala
+++ src/main/scala/net/debasishg/snippet/cake/Main.scala
@@ -1,13 +1,14 @@
 package net.debasishg.snippet.cake
 
 import java.util.Calendar.getInstance
+import java.util.Date
 
 object Main extends App {
   val account = Account("100", "dg", getInstance.getTime, "a")
 
-  val audit1 = ClientPortfolioAuditService1.currentPortfolio(account)
+  val audit1: List[(Double, Currency, Date)] = ClientPortfolioAuditService1.currentPortfolio(account)
   println(s"audit1=$audit1")
 
-  val audit2 = ClientPortfolioAuditService2.currentPortfolio(account)
+  val audit2: List[ClientPortfolioAuditService2.bal.BalanceRep] = ClientPortfolioAuditService2.currentPortfolio(account)
   println(s"audit2=$audit2")
 }
